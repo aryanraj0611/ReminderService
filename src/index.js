2854b3ba4190dabec1180fe6bc082237d8aef87c
aryanraj0611 <aryanraj01010@gmail.com>
2026-04-06 01:24:14 +0530

    added notification ticket model

--- src/index.js
+++ src/index.js
@@ -3,7 +3,9 @@ const bodyParser = require('body-parser');
 
 const {PORT} = require('./config/serverConfig')
 
-const {sendBasicEmail} = require('./services/email-service')
+//const {sendBasicEmail} = require('./services/email-service')
+
+const cron = require('node-cron')
 
 const setupAndStartServer = () =>{
     const app =express();
@@ -13,12 +15,16 @@ const setupAndStartServer = () =>{
     app.listen(PORT, () =>{
         console.log("Server started at PORT ",PORT);
 
-        sendBasicEmail(
-            'support@admin.com',
-            'remindernotificationservice@gmail.com',
-            'This is a testing email',
-            'hey how are you, i hope you like the support service'
-        )
+        // sendBasicEmail(
+        //     'support@admin.com',
+        //     'remindernotificationservice@gmail.com',
+        //     'This is a testing email',
+        //     'hey how are you, i hope you like the support service'
+        // )
+
+        cron.schedule('*/2 * * * *', () => {
+        console.log('running a task every two minute');
+});
     })
 }
 
